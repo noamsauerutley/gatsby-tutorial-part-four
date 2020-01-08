761d95eb1dd9e68e8f6db5f101de741fda9221de
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-01-07 19:29:35 -0500

    Done.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,30 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
+module.exports = {
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+    description: `A simple description about pandas eating lots...`,
+    author: `gatsbyjs`,
+  },
+  plugins: [
+    {
+    resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatbsyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatbsy-plugin-react-helmet`,
+  ],
+}
+
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`){
@@ -14,6 +38,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 }
 
 exports.createPages = async ({ graphql, actions }) => {
+
 const { createPage } = actions
 const result = await graphql(`
     query {
